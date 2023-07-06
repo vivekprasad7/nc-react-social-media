9d10346074418ea84f59b2b57b8cdb8fc1758803
Vivek Prasad <flashtech007@gmail.com>
2023-07-07 02:36:25 +0530

    refactor - added handlers in userContext

--- src/contexts/userContext.js
+++ src/contexts/userContext.js
@@ -2,9 +2,11 @@ import { useReducer } from "react";
 import { useContext } from "react";
 import { createContext } from "react";
 import { toast } from "react-hot-toast";
-import { followUserService, getAllUsersService, unfollowUserService } from "../services/dataFetchServices";
+import { addBookmarkService, editUserService, followUserService, getAllBookmarksService, getAllUsersService, removeBookmarkService, unfollowUserService } from "../services/dataFetchServices";
 import { useState } from "react";
 import { useAuthContext } from "./authContext";
+import { useEffect } from "react";
+import { userReducer } from "../reducers/userReducer";
 
 const UserContext = createContext();
 
@@ -17,9 +19,10 @@ export const UserContextProvider = ({children}) => {
         user:{},
     }
 
-    const [userState, userDispatch] = useReducer(useReducer, initialUserState);
+    const [userState, userDispatch] = useReducer(userReducer, initialUserState);
     const {authState, authDispatch} = useAuthContext();
     const [ userLoading, setUserLoading] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
 
     const getAllUsers = async () => {
         try{
@@ -30,6 +33,21 @@ export const UserContextProvider = ({children}) => {
                 setUserLoading(false);
             }
 
+        } catch(e){
+            console.error(e);
+            toast.error(e.response.data.errors[0]);
+        }
+    }
+
+    const editUserHandler = async(userData) => {
+        try{
+            const {data, status} = await editUserService(userData, authState?.token);
+
+            if(status === 201){
+                userDispatch({type:"UPDATE_USER_DATA", payload: data?.user});
+                authDispatch({type:"SET_USER", payload: data?.user});
+                toast.success("Profile Updated!")
+            }
         } catch(e){
             console.error(e);
             toast.error(e.response.data.errors[0]);
@@ -69,13 +87,75 @@ export const UserContextProvider = ({children}) => {
         }
     }
 
-    
+        const getAllBookmarks = async() => {
+            setIsLoading(true);
+            try{
+                const {data, status} = await getAllBookmarksService();
+                if(status === 200){
+                    userDispatch({type: "GET_ALL_BOOKMARKS", payload: data?.bookmarks})
+                    setIsLoading(false);
+                }
+            } catch(e) {
+                console.error(e);
+                toast.error(e.response.data.errors[0]);
+                setIsLoading(false);
+            }
+        }
 
+
+        const addBookmarkHandler = async (postID) =>{
+            setIsLoading(true);
+            try{
+                const {data, status} = await addBookmarkService(postID, authState?.token);
+                if( status === 200 || status === 201){
+                    userDispatch({type: "ADD_BOOKMARK", payload: data?.bookmarks});
+                    setIsLoading(false);
+                    toast.success("Added to Bookmarks!")
+                }
+            } catch(e){
+                console.error(e);
+                toast.error(e.response.data.errors[0]);
+                setIsLoading(false);
+            }
+        }
+
+        const removeBookmarkHandler = async (postID) => {
+            setIsLoading(true);
+            try{
+                const {data, status} = await removeBookmarkService(postID, authState?.token);
+                if(status ===200 || status === 201){
+                    userDispatch({type:"REMOVE_BOOKMARK", payload: data?.bookmarks});
+                    setIsLoading(false);
+                    toast.success("Removed from Bookmark!")
+                }
+            } catch(e){
+                console.error(e);
+                toast.error(e.response.data.errors[0]);
+                setIsLoading(false);
+            }
+        }
+
+
+
+        useEffect(()=> {
+            authState?.token && getAllUsers();
+          
+        }, [authState?.token])
 
 
 
     return(
-        <UserContext.Provider>{children}</UserContext.Provider>
+        <UserContext.Provider value={{
+            userState,
+            userDispatch,
+            getAllUsers,
+            editUserHandler,
+            followUserHandler,
+            unfollowUserHandler,
+            getAllBookmarks,
+            addBookmarkHandler,
+            removeBookmarkHandler,
+        }}>{children}</UserContext.Provider>
     )
 }
 
