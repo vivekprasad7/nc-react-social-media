f3bb1577379d6d44c2a3ec5219acea578cd8a2ad
Vivek Prasad <flashtech007@gmail.com>
2023-07-10 03:55:28 +0530

    feat - added followingUserPosts function

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,10 +4,18 @@ import { Sidebar } from '../../components/sidebar/Sidebar'
 import { Feed } from '../../components/feed/Feed'
 import { Widgets } from '../../components/widgets/Widgets'
 import { usePostContext } from '../../contexts/postContext'
+import { useAuthContext } from '../../contexts/authContext'
 
 export const Home = () => {
   const {postState, isLoading} = usePostContext();
+  const {authState} = useAuthContext();
   const {posts} = postState;
+
+  const followingUserPosts = postState?.posts?.filter((post) => 
+    authState?.user?.following?.some((user) => user?.username === authState?.user?.username) 
+    || post?.username === authState?.user?.username
+  )
+
   return (
     <div className='home-page'>
         <Sidebar/>
