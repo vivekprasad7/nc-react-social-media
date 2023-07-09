9b539478e07efb44cde87887ab1284a31c15dbff
Vivek Prasad <flashtech007@gmail.com>
2023-07-10 04:36:21 +0530

    refactor - explore page

--- src/pages/explore/Explore.jsx
+++ src/pages/explore/Explore.jsx
@@ -3,12 +3,14 @@ import "./Explore.css"
 import { Sidebar } from '../../components/sidebar/Sidebar'
 import { Feed } from '../../components/feed/Feed'
 import { Widgets } from '../../components/widgets/Widgets'
+import { usePostContext } from '../../contexts/postContext'
 
 export const Explore = () => {
+  const {postState, isLoading} = usePostContext();
   return (
     <div className='explore-page'>
     <Sidebar/>
-   <Feed pageTitle={"Explore"}/>
+   <Feed pageTitle={"Explore"} feedPosts={postState?.posts} isLoading={isLoading}/>
     <Widgets/>
 
 </div>
